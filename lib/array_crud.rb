3f31aa4b8a5230ae22204a68e39a74a554aae7a0
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-17 22:04:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
   
 def add_element_to_end_of_array(array, element)
-  array.append('a')
+  array.append('arrays!')
 end
 
 def add_element_to_start_of_array(array, element)
-  array.upshift('a')
+  array.unshift('arrays!')
 end
 
 def remove_element_from_end_of_array(array)
